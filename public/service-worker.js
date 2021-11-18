ec1ef609a0aa8aa43c0657a6b9a1c7b48f5c31a7
Kiri <kirilynne@gmail.com>
2021-11-17 22:38:34 -0500

    add files to cache to service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const FILES_TO_CACHE = [
+    "/",
+    "index.html",
+    "styles.css",
+    "db.js",
+    "index.js",
+    "manifest.json",
+    "service-worker.js",
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
