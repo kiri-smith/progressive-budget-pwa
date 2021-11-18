9630f98ca833d684c0087b67fb77ff95802c4e92
Kiri <kirilynne@gmail.com>
2021-11-17 23:03:05 -0500

    add get requests to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,3 +42,30 @@ self.addEventListener("activate", function (event) {
             .then(() => self.clients.claim())
     );
 });
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(event.request);
+                        });
+                }).catch(err => console.log(err))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
