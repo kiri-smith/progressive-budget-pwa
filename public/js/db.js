8e29fbf8116b8014e2662b9c768046e76ed8d4d3
Kiri <kirilynne@gmail.com>
2021-11-17 22:19:28 -0500

    add save record function to db.js

--- public/js/db.js
+++ public/js/db.js
@@ -2,23 +2,29 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-  event.target.result.createObjectStore("pending", 
-  {
-    keyPath: "id",
-    autoIncrement: true
-  }
-  );
+    event.target.result.createObjectStore("pending",
+        {
+            keyPath: "id",
+            autoIncrement: true
+        }
+    );
 };
 
 request.onsuccess = function (event) {
     db = event.target.result;
-  
+
     if (navigator.onLine) {
-      checkDatabase();
+        checkDatabase();
     }
-  };
+};
 
 request.onerror = function (err) {
-  console.log(err.message);
+    console.log(err.message);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
 
